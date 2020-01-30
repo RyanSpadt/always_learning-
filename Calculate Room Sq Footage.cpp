cb69cc46edd3669fd48b5b30f68d8fbf7ec49cbe
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-01-30 01:18:57 -0500

    Add files via upload
    This CPP program displays the syntax for initializing C++11 variables. It also takes user input of the width and length of a room, and displays the area in sq feet.

---
+++ Calculate Room Sq Footage.cpp
@@ -0,0 +1,27 @@
+// Project4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+
+using namespace std;
+
+// This program will calculate the area of a room in square feet
+
+int main()
+{
+
+    // Variable Declarations
+    int room_width{ 0 };
+    int room_length{ 0 };
+
+    // Get user inputs
+    cout << "Enter the width of the room: ";
+    cin >> room_width;
+
+    cout << "Enter the length of the room: ";
+    cin >> room_length;
+
+    cout << "The area of the room is "
+         << room_width * room_length << " square feet.";
+
+    return 0;
+}
