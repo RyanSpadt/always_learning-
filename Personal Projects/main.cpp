8333a92a00d6424d1632233add6beeb18b9a9e4a
Ryan S <60458664+RyanSpadt@users.noreply.github.com>
2020-12-18 11:55:45 -0500

    Upload main.cpp file
    For Project GPA calculator updated

---
+++ Personal Projects/main.cpp
@@ -0,0 +1,17 @@
+/*
+    Author: Ryan Spadt
+    Revisions: 1.0
+    Date: 18 December 2020
+*/
+
+#include "GPA.h"
+
+int main()
+{
+    GPA gpa;
+
+    gpa.getInput();
+    gpa.calculateGPA();
+
+    return 0;
+}
