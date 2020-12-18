45a3d5cfc2533ec50907b1016aa829e582cace25
Ryan S <60458664+RyanSpadt@users.noreply.github.com>
2020-12-18 12:00:38 -0500

    Update GPA.cpp
    add comments

--- Personal Projects/GPA Calculator Updated/GPA.cpp
+++ Personal Projects/GPA Calculator Updated/GPA.cpp
@@ -19,9 +19,11 @@ void GPA::getInput()
 	cout << "\nPlease enter how many classes you wish to calculate for: ";
 	cin >> class_amount;
 
+	// local
 	string grade_input;
 	float credit_input;
 
+	// get grades and credit weight for courses
 	for (unsigned int i{ 1 }; i <= class_amount; i++)
 	{
 		cout << "\nClass " << i << " Grade: ";
@@ -35,14 +37,18 @@ void GPA::getInput()
 }
 
 
+// Goes through all of the vector elements comparing the grade to upper/lower case version in the local arrays
+// Once it finds what it is equivalent to it grabs the quality points at the same index and calculates the GPA
 void GPA::calculateGPA()
 {
+	// local
 	float grade_point{ 0.00f }, credit_hours{ 0.00f };
 	string possible_gradesU[9]{ "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "F" };
 	string possible_gradesL[9]{ "a", "a-", "b+", "b", "b-", "c+", "c", "c-", "f" };
 	float possible_quality_points[9]{ 4.00f, 3.70f, 3.33f, 3.00f, 2.70f, 2.30f, 2.00f, 1.70f, 0.00f };
 	unsigned int x{ 0 };
 
+	// Loops through all of the grades in the vector and also loops through the local arrays time complexity O(n^2)
 	for (unsigned int i{ 0 }; i < grades.size(); i++)
 	{
 
@@ -66,6 +72,7 @@ void GPA::calculateGPA()
 }
 
 
+// Prints all elements of our two vectors
 void GPA::printAll()
 {
 	for (unsigned int i{ 0 }; i < grades.size(); i++)
